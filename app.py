54f75ce95df8b93b6965beb4145eb33bcce8d3e1
Nicholas Wolf <nickwolf49@gmail.com>
2021-03-13 21:39:32 -0500

    Implementation of proof of concept image segment token identifier

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session
 import os
-from utils.db_handlers import fetch_new_segment, record_transcription
+from utils.db_handlers import fetch_new_segment, record_transcription, record_user_strokes
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -16,9 +16,17 @@ def transcriber_home():
 @app.route('/addrec',methods = ['POST', 'GET'])
 def addrec():
     msg = record_transcription(request.form['segment_transcription'], session.get('current_row_id', None))
-    return render_template("thanks.html", msg=msg)
+    list_stroke_coords = request.form['segment_coords'].split('|')[:-1]
+    if len(list_stroke_coords):
+        for stroke_coords in list_stroke_coords:
+            insert_row = tuple([int(i) for i in stroke_coords.split('-')]) + (session.get('current_row_id', None),)
+            print(insert_row)
+            stroke_coords_msg = record_user_strokes(insert_row)
+    else:
+        stroke_coords_msg = "No segments produced."
+    return render_template("thanks.html", msg=msg + '. ' + stroke_coords_msg)
     con.close()
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='127.0.0.1', debug = True)
--- utils/db_handlers.py
+++ utils/db_handlers.py
@@ -3,18 +3,32 @@ import sqlite3
 def fetch_new_segment():
     with sqlite3.connect("transcriptions.db") as con:
         cur = con.cursor()
-        cur.execute("""SELECT * FROM segments WHERE complete = 0 LIMIT 1""")
+        cur.execute("""SELECT * FROM segments WHERE complete = 0 AND checkout = 0 LIMIT 1""")
         rows = cur.fetchall()
-        return rows[0][0], rows[0][5]
+        filename = rows[0][0]
+        id = rows[0][5]
+        #cur.execute("""UPDATE segments SET checkout = 1 WHERE id = ?""", (id,))
+        return filename, id
 
 def record_transcription(transcription, row_num):
     try:
         with sqlite3.connect("transcriptions.db") as con:
             cur = con.cursor()
-            cur.execute("""INSERT INTO transcriptions (transcription) VALUES (?)""",(transcription,) )
-            cur.execute("""UPDATE segments SET complete = 1 where id = ? """, (row_num,))
+            cur.execute("""INSERT INTO transcriptions (segment_id, transcription) VALUES (?,?)""",(row_num, transcription) )
+            cur.execute("""UPDATE segments SET complete = 0 WHERE id = ? """, (row_num,))  ## Set complete to 1 when ready to start up again
             con.commit()
         return "Record successfully added"
+    except:
+        con.rollback()
+        return "Error in insert operation"
+
+def record_user_strokes(id_plus_coords):
+    try:
+        with sqlite3.connect("transcriptions.db") as con:
+            cur = con.cursor()
+            cur.execute("""INSERT INTO user_stroke_coordinates (x1_coord, y1_coord, x2_coord, y2_coord, segment_id) VALUES (?,?,?,?,?)""",(id_plus_coords) )
+            con.commit()
+        return "User-provided stroke segments added to database."
     except:
         con.rollback()
         return "Error in insert operation"
